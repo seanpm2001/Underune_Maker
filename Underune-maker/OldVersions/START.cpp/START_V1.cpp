8060c58234a97c84fe8fa57973a11269974eeb5d
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-08-27 11:50:43 -0700

    START_V1.cpp
    An archive of the first version of the C++ startup script for Underrune maker.

---
+++ Underune-maker/OldVersions/START.cpp/START_V1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int start() {
+  cout << "Underrune maker";
+  cout << "The Undertale and Deltarune map maker";
+  return 0;
+}
+
+int main() {
+  start();
+  return 0;
+}
+/* File info
+* This is still very much a work in progress. I also decided to write this in a language I know less about (C++)
+* to match what the engine the original games were made in (gamemaker)
+* File type: C++ file (*.cpp)
+* Line count (including blank lines and compiler line): 22
+* File version: 1 (Thursday, August 27th 2020 at 11:50 am)
+*/
+// End of script
