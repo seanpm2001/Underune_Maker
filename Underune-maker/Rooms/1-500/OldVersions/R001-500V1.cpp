e040cad97c705bd8899b9ea43f8cab1356fb7be1
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-08-27 18:00:02 -0700

    R001-500V1.cpp
    An archive of the first version of the script file for the first 500 rooms.

---
+++ Underune-maker/Rooms/1-500/OldVersions/R001-500V1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+// This is the script file for rooms 1 to 500
+int main() {
+  cout << "Rooms 1 to 500";
+  return 0;
+} 
+// False = locked | True = unlocked
+// Rooms 1 to 10
+room1key = bool(true); 
+room1val = int(001);
+room1Type = str("Ruins");
+room1x = int(50);
+room1y = int(50);
+room2key = bool(true); 
+room2val = int(002);
+room2Type = str("Ruins");
+room2x = int(50);
+room2y = int(50);
+room3key = bool(true); 
+room3val = int(003);
+room3Type = str("Ruins");
+room3x = int(50);
+room3y = int(50);
+room4key = bool(true); 
+room4val = int(004);
+room4Type = str("Ruins");
+room4x = int(50);
+room4y = int(50);
+room5key = bool(true); 
+room5val = int(005);
+room5Type = str("Ruins");
+room5x = int(50);
+room5y = int(50);
+room6key = bool(true); 
+room6val = int(006);
+room6Type = str("Ruins");
+room6x = int(50);
+room6y = int(50);
+room7key = bool(true); 
+room7val = int(007);
+room7Type = str("Ruins");
+room7x = int(50);
+room7y = int(50);
+room8key = bool(true); 
+room8val = int(008);
+room8Type = str("Ruins");
+room8x = int(50);
+room8y = int(50);
+room9key = bool(true); 
+room9val = int(009);
+room9Type = str("Ruins");
+room9x = int(50);
+room9y = int(50);
+room10key = bool(true); 
+room10val = int(010);
+room10Type = str("Ruins");
+room10x = int(50);
+room10y = int(50);
+// Rooms 11 to 20
+/* Coming soon */
+// More rooms coming soon
+// Placeholder
+/* File info
+* File type: C++ file (*.cpp)
+* File version: 1 (Thursday, August 27th 2020 at 5:59 pm)
+* Line count (including blank lines and compiler line): 70
+*/
+// End of script
