86b91d18cee86d0d6d415ef59a8492951ad0d506
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2025-05-23 17:18:39 -0700

    1 sub-directory [2 files]
    Initial route configuration data for the game.

---
+++ Underune-maker/Game-Config/Route/!OldVersions/1/1-100/Route_V1.cpp
@@ -0,0 +1,34 @@
+// Start of script
+// Underune Maker
+// Route.cpp
+// Defines the type of game (Pacifist, Neutral, or Genocide)
+// By @seanpm2001
+// TODO C++ source code is not yet valid
+#include <iostream>
+using namespace std;
+int main() {
+    // = 0 : Pacifist
+    // = 1 : Neutral
+    // = 2 : Genocide
+    string routeType = "Pacifist";
+    int route0 << 0;
+    if route0 << [0] {
+        routeType << "Pacifist"; // Pacifist: Go through the game without killing any monsters
+    }
+    else if route0 << [1] {
+        routeType << "Neutral"; // Neutral: Go through the game, and kill some (but not all) monsters
+    }
+    else {
+        routeType << "Genocide"; // Genocide: Go through the game, and kill every monster
+    }
+}
+return main();
+return 0;
+break;
+/* File info
+File type: C++ source file (*.cpp)
+File version: 1 (2025, Friday, May 23rd at 12:55 pm PST)
+Line count (including blank lines and compiler line): 34
+*/
+// End of script
+
